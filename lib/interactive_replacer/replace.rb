48794f9561bd7e5fe1804af29b48ba9f553322af
motoki-shun <motoki-shun@kayac.com>
2017-05-01 17:21:04 +0900

    replaceいったん

--- lib/interactive_replacer/replace.rb
+++ lib/interactive_replacer/replace.rb
@@ -1,6 +1,42 @@
 module InteractiveReplacer
   class Replace
+    def initialize(search)
+      @search = search
+    end
+
+    def replace_all(path, before, after='')
+    end
+
+    def replace_directory(path, before, after='')
+    end
+
+    def replace_filename(path, before, after='')
+    end
+
+    def replace_in_file_recursive(path, before, after='')
+      target_file_paths(path).each do |file_path|
+        replace_in_file(file_path, before, after)
+      end
+    end
+
+    def replace_in_file(file_path, before, after='')
+      txt = File.read(file_path).gsub(before, after)
+      File.write(file_path, txt)
+    end
+
     def replace_in_file_interactive(file_path, before, after='')
+    end
+
+    private
+
+    def target_file_paths(path)
+      paths = Dir.glob "#{path}/**/*"
+      paths.reject { |path| File.directory?(path) }
+    end
+
+    def target_directory_paths(path)
+      paths = Dir.glob "#{path}/**/*"
+      paths.select { |path| File.directory?(path) }
     end
   end
 end
