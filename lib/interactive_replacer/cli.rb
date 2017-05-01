a5639649d6861f2e1b9fcdc1827f2cd5a99f5e71
motoki-shun <motoki-shun@kayac.com>
2017-05-01 17:38:26 +0900

    いったん適当にオプション追加

--- lib/interactive_replacer/cli.rb
+++ lib/interactive_replacer/cli.rb
@@ -8,7 +8,16 @@ module InteractiveReplacer
         opt.on('--interactive') { |v| options[:interactive] = v }
         opt.on('--directory VAL') { |v| options[:directory] = v }
         opt.on('--file VAL') { |v| options[:file] = v }
-        opt.on('-m VAL') { |v| options[:method] = v }
+        opt.on('--ignore-case') { |v| options[:ignore_case] = true }
+        opt.on('--count') { |v| options[:count] = true }
+        opt.on('--files-without-matches') { |v| options[:files_without_matches] = true }
+        opt.on('--literal') { |v| options[:literal] = true }
+        opt.on('--regexp') { |v| options[:regexp] = true }
+        opt.on('--smart-case') { |v| options[:smart_case] = true }
+        opt.on('--directory') { |v| options[:directory] = true }
+        opt.on('--file') { |v| options[:file] = true }
+        opt.on('--show-hidden-files') { |v| options[:show_hidden_files] = true }
+        opt.on('--dry-run') { |v| options[:dry_run] = true }
 
         opt.parse!(ARGV)
       end
