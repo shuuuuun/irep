5d48dbb0de2e3a67936916ffaab18d44e4860c19
motoki-shun <motoki-shun@kayac.com>
2017-05-01 23:34:53 +0900

    interactiveにreplaceできた！

--- lib/interactive_replacer/replace.rb
+++ lib/interactive_replacer/replace.rb
@@ -31,7 +31,43 @@ module InteractiveReplacer
     end
 
     def replace_in_file_interactive(file_path, before, after='')
+      file_text = File.read(file_path)
+      # puts gets, file_path, before, after
+      @search.results.each do |result|
+        puts result[:preview]
+        print 'Replace [y,n,q,a,d,/,j,J,g,e,?]? '
+        cmd = gets.chomp
+        case cmd
+        when 'y'
+          puts result
+          result[:should_replace] = true
+          # replace_one_word(file_text, result[:offset], before, after)
+          # 文字列を特定位置で分割して配列にしたい
+          # first_text = file_text.slice(0..(result[:offset] - 1))
+          # last_text = file_text.slice(result[:offset]..file_text.size)
+          # last_text.sub(before, after)
+          # replaced_text = first_text + last_text
+          # p first_text, last_text
+          # File.write(file_path, replaced_text)
+          # ２回めのreplaceで1回目のがなかったことになってしまう
+        end
+      end
+      replaced_text = file_text.split(before).map.with_index do |text, index|
+        result = @search.results[index]
+        if !result
+          text
+        elsif result.fetch(:should_replace, nil)
+          text + after
+        else
+          text + before
+        end
+      end.join('')
+      File.write(file_path, replaced_text)
     end
+
+    # def replace_one_word(text, offset, before, after='')
+    #   text
+    # end
 
     private
 
