42f55c1c37f43bff30ea5272f3b935de8e2bcc81
motoki-shun <motoki-shun@kayac.com>
2017-05-01 18:26:36 +0900

    cli:parse部分調整

--- lib/interactive_replacer/cli.rb
+++ lib/interactive_replacer/cli.rb
@@ -2,27 +2,39 @@ require 'optparse'
 
 module InteractiveReplacer
   class CLI
-    def self.execute(stdout, arguments = [])
-      options = {}
-      opt = OptionParser.new do |opt|
-        opt.on('--interactive') { |v| options[:interactive] = v }
-        opt.on('--directory VAL') { |v| options[:directory] = v }
-        opt.on('--file VAL') { |v| options[:file] = v }
-        opt.on('--ignore-case') { |v| options[:ignore_case] = true }
-        opt.on('--count') { |v| options[:count] = true }
-        opt.on('--files-without-matches') { |v| options[:files_without_matches] = true }
-        opt.on('--literal') { |v| options[:literal] = true }
-        opt.on('--regexp') { |v| options[:regexp] = true }
-        opt.on('--smart-case') { |v| options[:smart_case] = true }
-        opt.on('--directory') { |v| options[:directory] = true }
-        opt.on('--file') { |v| options[:file] = true }
-        opt.on('--show-hidden-files') { |v| options[:show_hidden_files] = true }
-        opt.on('--dry-run') { |v| options[:dry_run] = true }
+    def self.execute(stdout, argv = [])
+      # puts "argv: #{argv}"
+      opts, args = parse_options argv
+      # stdout.print op.help
+      puts "opts: #{opts}"
+      puts "args: #{args}"
+    end
 
-        opt.parse!(ARGV)
+    def self.parse_options(argv = [])
+      options = {}
+
+      op = OptionParser.new
+      op.on('--interactive') { |v| options[:interactive] = v }
+      op.on('--directory VAL') { |v| options[:directory] = v }
+      op.on('--file VAL') { |v| options[:file] = v }
+      op.on('--ignore-case') { |v| options[:ignore_case] = true }
+      op.on('--count') { |v| options[:count] = true }
+      op.on('--files-without-matches') { |v| options[:files_without_matches] = true }
+      op.on('--literal') { |v| options[:literal] = true }
+      op.on('--regexp') { |v| options[:regexp] = true }
+      op.on('--smart-case') { |v| options[:smart_case] = true }
+      op.on('--show-hidden-files') { |v| options[:show_hidden_files] = true }
+      op.on('--dry-run') { |v| options[:dry_run] = true }
+
+      begin
+        # op.parse!(argv)
+        arguments = op.parse(argv)
+      rescue OptionParser::InvalidOption => e
+        # usage e.message
+        puts "error: #{e.message}"
       end
 
-      stdout.print opt.help
+      [options, arguments]
     end
   end
 end
