a8595235933213b766e4ee16ea7fa68cbd59431d
motoki-shun <motoki-shun@kayac.com>
2017-05-27 16:08:30 +0900

    ユーザー入力をクラスメソッドに

--- lib/interactive_replacer/interface.rb
+++ lib/interactive_replacer/interface.rb
@@ -12,13 +12,17 @@ module InteractiveReplacer
         puts opts[:preview]
         # print 'Replace [y,n,q,a,d,/,j,J,g,e,?]? '
         print "#{@message} [#{@cases.map {|c| c[:cmd]}.join(',')}]? "
-        cmd = STDIN.gets.chomp
+        cmd = self.class.get_input
         match_case = @cases.select {|c| c[:cmd] == cmd.downcase }.first
         if match_case
           match_case[:func].call(*opts[:proc_args]) if match_case[:func]
           flag = false
         end
       end
+    end
+
+    def self.get_input
+      STDIN.gets.chomp
     end
   end
 end
