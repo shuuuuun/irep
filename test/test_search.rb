64b19cfbe82a83cfe01297e5b5e4e19594c2d59b
motoki-shun <motoki-shun@kayac.com>
2017-05-27 20:53:37 +0900

    fix test

--- test/test_search.rb
+++ test/test_search.rb
@@ -10,43 +10,48 @@ class TestSearch < Test::Unit::TestCase
   end
 
   def test_find_directory
-    directory = 'test_find_directory'
-    FileUtils.mkdir_p File.join(@tmp_dir, directory)
+    directory_1 = 'test_find_directory_1'
+    directory_2 = 'test_find_directory_2'
+    FileUtils.mkdir_p File.join(@tmp_dir, directory_1)
+    FileUtils.mkdir_p File.join(@tmp_dir, directory_2)
 
     search = InteractiveReplacer::Search.new directory: @tmp_dir, search_text: 'test_find_directory'
     search.find_directory
 
     result = search.results[0]
-    assert_equal search.results.size, 1
+    assert_equal search.results.size, 2
     assert_equal result[:type], 'directory'
-    assert_equal result[:path], File.join(@tmp_dir, directory)
+    assert_equal result[:path], File.join(@tmp_dir, directory_1)
   end
 
   def test_find_filename
-    filename = 'test_find_filename.txt'
-    create_test_data(filename, '')
+    filename_1 = 'test_find_filename_1.txt'
+    filename_2 = 'test_find_filename_2.txt'
+    create_test_data(filename_1, '')
+    create_test_data(filename_2, '')
 
     search = InteractiveReplacer::Search.new directory: @tmp_dir, search_text: 'test_find_filename'
     search.find_filename
 
     result = search.results[0]
-    assert_equal search.results.size, 1
+    assert_equal search.results.size, 2
     assert_equal result[:type], 'filename'
-    assert_equal result[:path], File.join(@tmp_dir, filename)
+    assert_equal result[:path], File.join(@tmp_dir, filename_1)
   end
 
   def test_find_in_file
     filename = 'test_find_in_file.txt'
-    text = 'test text'
+    file_path = File.join(@tmp_dir, filename)
+    text = 'test text test'
     create_test_data(filename, text)
 
-    search = InteractiveReplacer::Search.new directory: @tmp_dir, search_text: text
-    search.find_in_file(filename)
+    search = InteractiveReplacer::Search.new directory: @tmp_dir, search_text: 'test'
+    search.find_in_file(file_path)
 
     result = search.results[0]
-    assert_equal search.results.size, 1
+    assert_equal search.results.size, 2
     assert_equal result[:type], 'in_file'
-    assert_equal result[:path], filename
+    assert_equal result[:path], file_path
     assert_equal result[:offset], 0
     assert_equal result[:line], 1
     assert_equal result[:colmun], 0
